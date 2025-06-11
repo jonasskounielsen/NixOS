bd5411ec6cd57752ab24fc1f33bd82337f12ec6b
jonas <jonasskou.n@gmail.com>
2025-06-11 18:01:57 +0200

    vscodium configuration

--- home.nix
+++ home.nix
@@ -41,8 +41,55 @@
                         usernamehw.errorlens
                     ];
                     userSettings = {
-
+                        "workbench.colorTheme" = "monokai";
+                        "editor.formatOnSave" = true;
+                        "editor.formatOnSaveMode" = "modifications";
+                        "git.autofetch" = true;
+                        "git.confirmSync" = false;
+                        "git.enableSmartCommit" = true;
+                        "explorer.confirmDelete" = false;
+                        "rust-analyzer.diagnostics.styleLints.enable" = true;
+                        "terminal.integrated.tabs.enabled" = false;
+                        "explorer.confirmDragAndDrop" = false;
                     };
+                    keybindings = [
+                        {
+                            key = "ctrl+alt+v";
+                            command = "-workbench.action.editorDictation.start";
+                            when = "hasSpeechProvider && !editorReadonly && !speechToTextInProgress";
+                        }
+                        {
+                            key = "ctrl+shift+d";
+                            command = "editor.action.addSelectionToPreviousFindMatch";
+                        }
+                        {
+                            key = "ctrl+k ctrl+shift+d";
+                            command = "editor.action.moveSelectionToPreviousFindMatch";
+                        }
+                        {
+                            key = "ctrl+'";
+                            command = "editor.action.commentLine";
+                            when = "editorTextFocus && !editorReadonly";
+                        }
+                        {
+                            key = "ctrl+shift+7";
+                            command = "-editor.action.commentLine";
+                            when = "editorTextFocus && !editorReadonly";
+                        }
+                        {
+                            key = "ctrl+shift+a";
+                            command = "-editor.action.blockComment";
+                            when = "editorTextFocus && !editorReadonly";
+                        }
+                        {
+                            key = "ctrl+shift+'";
+                            command = "editor.action.blockComment";
+                        }
+                        {
+                            key = "ctrl+alt+s";
+                            command = "workbench.action.files.saveFiles";
+                        }
+                    ];
                 };
             };
         };
