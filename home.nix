2a7c9f39c838745e3eeaa601f8efac65c6d0c0e5
jonas <jonasskou.n@gmail.com>
2025-06-26 23:40:55 +0200

    add esp8266 profile, restructure

--- home.nix
+++ home.nix
@@ -7,7 +7,6 @@
         packages = with pkgs; [
             kdePackages.plasma-browser-integration
             discord
-            steam
             onedriver
             bitwarden-desktop
             onlyoffice-desktopeditors
@@ -33,63 +32,76 @@
         vscode = {
             enable = true;
             package = pkgs.vscodium;
-            profiles = {
+            profiles =
+            let
+                userSettings = {
+                    "editor.formatOnSave" = true;
+                    "editor.formatOnSaveMode" = "modifications";
+                    "git.autofetch" = true;
+                    "git.confirmSync" = false;
+                    "git.enableSmartCommit" = true;
+                    "explorer.confirmDelete" = false;
+                    "rust-analyzer.diagnostics.styleLints.enable" = true;
+                    "terminal.integrated.tabs.enabled" = false;
+                    "explorer.confirmDragAndDrop" = false;
+                    "workbench.colorTheme" = "Monokai";
+                };
+                keybindings = [
+                    {
+                        key = "ctrl+alt+v";
+                        command = "-workbench.action.editorDictation.start";
+                        when = "hasSpeechProvider && !editorReadonly && !speechToTextInProgress";
+                    }
+                    {
+                        key = "ctrl+shift+d";
+                        command = "editor.action.addSelectionToPreviousFindMatch";
+                    }
+                    {
+                        key = "ctrl+k ctrl+shift+d";
+                        command = "editor.action.moveSelectionToPreviousFindMatch";
+                    }
+                    {
+                        key = "ctrl+'";
+                        command = "editor.action.commentLine";
+                        when = "editorTextFocus && !editorReadonly";
+                    }
+                    {
+                        key = "ctrl+shift+7";
+                        command = "-editor.action.commentLine";
+                        when = "editorTextFocus && !editorReadonly";
+                    }
+                    {
+                        key = "ctrl+shift+a";
+                        command = "-editor.action.blockComment";
+                        when = "editorTextFocus && !editorReadonly";
+                    }
+                    {
+                        key = "ctrl+shift+'";
+                        command = "editor.action.blockComment";
+                    }
+                    {
+                        key = "ctrl+alt+s";
+                        command = "workbench.action.files.saveFiles";
+                    }
+                ];
+                extensions = with pkgs.vscode-extensions; [
+                    usernamehw.errorlens
+                ];
+            in {
                 rust = { # apparently home manager creates a broken extensions dir if there are no extensions
-                    extensions = with pkgs.vscode-extensions; [
+                    userSettings = userSettings;
+                    keybindings = keybindings;
+                    extensions = extensions ++ (with pkgs.vscode-extensions; [
                         rust-lang.rust-analyzer
                         vadimcn.vscode-lldb
-                        usernamehw.errorlens
-                    ];
-                    userSettings = {
-                        "editor.formatOnSave" = true;
-                        "editor.formatOnSaveMode" = "modifications";
-                        "git.autofetch" = true;
-                        "git.confirmSync" = false;
-                        "git.enableSmartCommit" = true;
-                        "explorer.confirmDelete" = false;
-                        "rust-analyzer.diagnostics.styleLints.enable" = true;
-                        "terminal.integrated.tabs.enabled" = false;
-                        "explorer.confirmDragAndDrop" = false;
-                        "workbench.colorTheme" = "Monokai";
-                    };
-                    keybindings = [
-                        {
-                            key = "ctrl+alt+v";
-                            command = "-workbench.action.editorDictation.start";
-                            when = "hasSpeechProvider && !editorReadonly && !speechToTextInProgress";
-                        }
-                        {
-                            key = "ctrl+shift+d";
-                            command = "editor.action.addSelectionToPreviousFindMatch";
-                        }
-                        {
-                            key = "ctrl+k ctrl+shift+d";
-                            command = "editor.action.moveSelectionToPreviousFindMatch";
-                        }
-                        {
-                            key = "ctrl+'";
-                            command = "editor.action.commentLine";
-                            when = "editorTextFocus && !editorReadonly";
-                        }
-                        {
-                            key = "ctrl+shift+7";
-                            command = "-editor.action.commentLine";
-                            when = "editorTextFocus && !editorReadonly";
-                        }
-                        {
-                            key = "ctrl+shift+a";
-                            command = "-editor.action.blockComment";
-                            when = "editorTextFocus && !editorReadonly";
-                        }
-                        {
-                            key = "ctrl+shift+'";
-                            command = "editor.action.blockComment";
-                        }
-                        {
-                            key = "ctrl+alt+s";
-                            command = "workbench.action.files.saveFiles";
-                        }
-                    ];
+                    ]);
+                };
+                esp8266 = {
+                    userSettings = userSettings;
+                    keybindings = keybindings;
+                    extensions = extensions ++ (with pkgs.vscode-extensions; [
+                        platformio.platformio-vscode-ide
+                    ]);
                 };
             };
         };
