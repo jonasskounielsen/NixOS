c27c93d5314d7f6c0e79c9dcb502ed95d9a05573
jonasskounielsen <jonasskou.n@gmail.com>
2025-07-21 20:17:55 +0200

    add kde connect, try to make ld work

--- devShells/ttt-masters.nix
+++ devShells/ttt-masters.nix
@@ -1,5 +1,7 @@
 { pkgs }:
-
+let
+    inherit (pkgs) lib stdenv;
+in
 pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
         cargo
@@ -7,5 +9,10 @@ pkgs.mkShell {
         rustfmt
         rustPackages.clippy
         libgcc
+        gdb
     ];
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc
+    ];
+    NIX_LD = builtins.readFile "${stdenv.cc}/nix-support/dynamic-linker";
 }
