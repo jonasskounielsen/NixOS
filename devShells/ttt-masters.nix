e772f5fd408bf2d1e272e48a90e41c8fa90b414c
jonas <jonasskou.n@gmail.com>
2025-08-14 21:43:50 +0200

    Add steam-run to ttt-masters devShell

--- devShells/ttt-masters.nix
+++ devShells/ttt-masters.nix
@@ -1,5 +1,9 @@
 { pkgs }:
 let
+    unfreePkgs = import pkgs.path {
+        inherit (pkgs) system overlays;
+        config = pkgs.config // { allowUnfree = true; };
+    };
     inherit (pkgs) lib;
 in
 pkgs.mkShell {
@@ -10,7 +14,9 @@ pkgs.mkShell {
         rustPackages.clippy
         libgcc
         gdb
-    ];
+    ] ++ (with unfreePkgs; [
+        steam-run
+    ]);
     LD_LIBRARY_PATH = lib.makeLibraryPath [
         pkgs.stdenv.cc.cc
     ];
