de8e59dc0a835420da1bd21f40c7535e5d2b1cb2
jonasskounielsen <jonasskou.n@gmail.com>
2025-08-21 15:24:57 +0200

    Add minesweeper shortcut

--- scripts/maple_shortcut.sh
+++ scripts/maple_shortcut.sh
@@ -1,7 +1,7 @@
 function maple() {
     export GDK_BACKEND=x11
     export _JAVA_AWT_WM_NONREPARENTING=1
-    distrobox-enter maple -- ~/maple2025/bin/xmaple
+    distrobox-enter maple -- "$HOME"/maple2025/bin/xmaple
     export GDK_BACKEND=""
     export _JAVA_AWT_WM_NONREPARENTING=""
 }
