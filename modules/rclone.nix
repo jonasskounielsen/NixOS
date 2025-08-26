53b2585e68543f8186f43de03ed8cc7f7c441dda
jonas <jonasskou.n@gmail.com>
2025-08-26 21:44:12 +0200

    Mount "Fag" subdirectory from Onedrive-skole rather than root dir

--- modules/rclone.nix
+++ modules/rclone.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 let
   homeDir = config.users.users.jonas.home;
-  rclone-service = remoteName: mountDir: {
+  rclone-service = remoteName: remotePath: mountDir: {
     description = "Rclone automount of Onedrive-skole";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
@@ -9,7 +9,7 @@ let
       Type = "notify";
       ExecStart = ''
         ${pkgs.rclone}/bin/rclone mount \
-          ${remoteName}: ${mountDir} \
+          ${remoteName}:${remotePath} ${mountDir} \
           --config ${homeDir}/.config/rclone/rclone.conf \
           --vfs-cache-mode=writes \
           --allow-other
@@ -23,8 +23,8 @@ let
 in
 {
   systemd.services."rclone-onedrive-skole" =
-    rclone-service "Onedrive-skole" "${homeDir}/Onedrive-skole";
+    rclone-service "Onedrive-skole" "/Fag" "${homeDir}/Onedrive-skole";
 
   systemd.services."rclone-onedrive-personlig" =
-    rclone-service "Onedrive-personlig" "${homeDir}/Onedrive-personlig";
+    rclone-service "Onedrive-personlig" "/" "${homeDir}/Onedrive-personlig";
 }
