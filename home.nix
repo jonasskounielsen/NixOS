b4e8f6194bb4ed86a0fb361d9f21cffffa7c68ca
jonas <jonasskou.n@gmail.com>
2025-10-02 20:06:24 +0200

    Move vscodium to module

--- home.nix
+++ home.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  imports = map
+    (module: "${./homeModules}/${module}")
+    (builtins.attrNames (builtins.readDir ./homeModules));
+
   home = {
     username = "jonas";
     homeDirectory = "/home/jonas";
@@ -33,127 +37,6 @@
       enable = true;
       userName = "jonasskounielsen";
       userEmail = "jonasskou.n@gmail.com";
-    };
-
-    vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-      profiles =
-        let
-          userSettings = {
-            "editor.formatOnSave" = true;
-            "editor.formatOnSaveMode" = "modifications";
-            "git.autofetch" = true;
-            "git.confirmSync" = false;
-            "git.enableSmartCommit" = true;
-            "explorer.confirmDelete" = false;
-            "rust-analyzer.diagnostics.styleLints.enable" = true;
-            "terminal.integrated.tabs.enabled" = false;
-            "explorer.confirmDragAndDrop" = false;
-            "workbench.colorTheme" = "Monokai";
-            "extensions.experimental.affinity" = {
-              "asvetliakov.vscode-neovim" = "1";
-            };
-          };
-          keybindings = [
-            {
-              key = "ctrl+shift+d";
-              command = "editor.action.addSelectionToPreviousFindMatch";
-            }
-            {
-              key = "ctrl+k ctrl+shift+d";
-              command = "editor.action.moveSelectionToPreviousFindMatch";
-            }
-            {
-              key = "ctrl+'";
-              command = "editor.action.commentLine";
-              when = "editorTextFocus && !editorReadonly";
-            }
-            {
-              key = "ctrl+shift+'";
-              command = "editor.action.blockComment";
-            }
-            {
-              key = "ctrl+alt+s";
-              command = "workbench.action.files.saveFiles";
-            }
-            {
-              key = "ctrl+enter";
-              command = "workbench.action.tasks.build";
-            }
-            {
-              key = "ctrl+enter";
-              command = "workbench.action.tasks.restartTask";
-              when = "taskRunning";
-            }
-            {
-              key = "ctrl+shift+w";
-              command = "-workbench.action.closeWindow";
-            }
-            {
-              key = "ctrl+shift+w";
-              command = "workbench.action.closeActiveEditor";
-            }
-            {
-              key = "ctrl-k ctrl+t";
-              command = "terminal.focus";
-            }
-          ];
-          extensions = with pkgs.vscode-extensions; [
-            usernamehw.errorlens
-            asvetliakov.vscode-neovim
-          ]; # apparently home manager creates a broken extensions dir if there are no extensions
-        in
-        {
-          rust = {
-            userSettings = userSettings;
-            keybindings = keybindings;
-            extensions =
-              extensions
-              ++ (with pkgs.vscode-extensions; [
-                rust-lang.rust-analyzer
-                vadimcn.vscode-lldb
-              ]);
-          };
-          esp8266 = {
-            userSettings = userSettings;
-            keybindings = keybindings;
-            extensions =
-              extensions
-              ++ (with pkgs.vscode-extensions; [
-                sumneko.lua
-              ]);
-          };
-          ttt-masters = {
-            userSettings = userSettings;
-            keybindings = keybindings;
-            extensions =
-              extensions
-              ++ (with pkgs.vscode-extensions; [
-                rust-lang.rust-analyzer
-                vadimcn.vscode-lldb
-              ]);
-          };
-          nix = {
-            userSettings = userSettings // {
-              "nix.enableLanguageServer" = true;
-              "nix.serverPath" = "nixd";
-              "nix.serverSettings" = {
-                "nixd" = {
-                  "formatting" = {
-                    "command" = [ "nixfmt" ];
-                  };
-                };
-              };
-            };
-            keybindings = keybindings;
-            extensions =
-              extensions
-              ++ (with pkgs.vscode-extensions; [
-                jnoortheen.nix-ide
-              ]);
-          };
-        };
     };
   };
 }
