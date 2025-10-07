9a9bb837d1daf5f50f017d7867091eebf8510e02
jonas <jonasskou.n@gmail.com>
2025-10-07 22:31:02 +0200

    Update Kitty config

--- homeModules/kitty.nix
+++ homeModules/kitty.nix
@@ -2,36 +2,53 @@
   programs.kitty = {
     enable = true;
     settings = {
-      font_family = "Monocraft"; # Does not work, probably because Minecraftia is not monospace.
-      bold_font = "Monocraft";
-      italic_font = "Monocraft";
+      clear_all_shortcuts = "yes";
+      "map ctrl+shift+c" = "copy_to_clipboard";
+      "map ctrl+shift+v" = "paste_from_clipboard";
+
+      "map ctrl+shift+k" = "scroll_line_up";
+      "map ctrl+shift+j" = "scroll_line_down";
+      "map ctrl+shift+page_up"   = "scroll_page_up";
+      "map ctrl+shift+page_down" = "scroll_page_down";
+      "map ctrl+shift+home" = "scroll_home";
+      "map ctrl+shift+end"  = "scroll_end";
+
+      "map ctrl+shift+t" = "launch --type tab --cwd current --location neighbor";
+      "map ctrl+alt+t"   = "launch --type tab --location neighbor";
+      "map ctrl+shift+h" = "previous_tab";
+      "map ctrl+shift+l" = "next_tab";
+      "map ctrl+shift+w" = "close_window";
+      
+      font_family      = "Monocraft";
+      bold_font        = "Monocraft";
+      italic_font      = "Monocraft";
       bold_italic_font = "Monocraft";
 
-      background          = "#191919";
-      foreground          = "#776b53";
-      cursor              = "#fac814";
-      selection_background= "#776b53";
-      color0              = "#321200";
-      color8              = "#423625";
-      color1              = "#b1270e";
-      color9              = "#ed5c20";
-      color2              = "#44a900";
-      color10             = "#55f237";
-      color3              = "#a9810b";
-      color11             = "#f1b731";
-      color4              = "#578499";
-      color12             = "#85cfec";
-      color5              = "#96363c";
-      color13             = "#e04b5a";
-      color6              = "#b2591d";
-      color14             = "#f07c14";
-      color7              = "#776b53";
-      color15             = "#ffc800";
-      selection_foreground= "#191919";
-      active_tab_foreground = "#eeeeee";
-      active_tab_background = "#776b53";
-      inactive_tab_foreground= "#776b53";
-      inactive_tab_background= "#141414";
+      background              = "#191919";
+      foreground              = "#776b53";
+      cursor                  = "#fac814";
+      selection_background    = "#776b53";
+      color0                  = "#321200";
+      color8                  = "#423625";
+      color1                  = "#b1270e";
+      color9                  = "#ed5c20";
+      color2                  = "#44a900";
+      color10                 = "#55f237";
+      color3                  = "#a9810b";
+      color11                 = "#f1b731";
+      color4                  = "#578499";
+      color12                 = "#85cfec";
+      color5                  = "#96363c";
+      color13                 = "#e04b5a";
+      color6                  = "#b2591d";
+      color14                 = "#f07c14";
+      color7                  = "#776b53";
+      color15                 = "#ffc800";
+      selection_foreground    = "#191919";
+      active_tab_foreground   = "#eeeeee";
+      active_tab_background   = "#776b53";
+      inactive_tab_foreground = "#776b53";
+      inactive_tab_background = "#141414";
     };
   };
 }
