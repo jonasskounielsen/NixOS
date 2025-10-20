20ace1c402cd7004d07453c804dc179b51d7dbc6
jonas <jonasskou.n@gmail.com>
2025-10-20 21:52:12 +0200

    Update Niri config

--- home-modules/niri.nix
+++ home-modules/niri.nix
@@ -1,7 +1,34 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
+let
+  brightnessctlScript = middle: ''
+    if [ -z $BRIGHTNESSCTL_BRIGHTNESS ]; then
+      BRIGHTNESSCTL_BRIGHTNESS=5
+    fi
+    ${middle}
+    values=("1%" "1.5%" "2.5%" "4%" "6%" "10%" "15%" "25%" "40%" "60%" "100%")
+    value="$\{values[$BRIGHTNESSCTL_BRIGHTNESS]}"
+    brightnessctl set --class=backlight "$value"
+  '';
+  variables = {
+    "$XWAYLAND_SATELLITE_PATH" = pkgs.xwayland-satellite;
+    "$BRIGHTNESSCTL_INCREASE" = brightnessctlScript ''
+      BRIGHTNESSCTL_BRIGHTNESS=(($BRIGHTNESSCTL_BRIGHTNESS+1))
+      if [ "$BRIGHTNESSCTL_BRIGHTNESS" -gt 10 ]; then
+        BRIGHTNESSCTL_BRIGHTNESS=10
+      fi
+    '';
+    "$BRIGHTNESSCTL_DECREASE" = brightnessctlScript ''
+      BRIGHTNESSCTL_BRIGHTNESS=(($BRIGHTNESSCTL_BRIGHTNESS-1))
+      if [ "$BRIGHTNESSCTL_BRIGHTNESS" -lt 0 ]; then
+        BRIGHTNESSCTL_BRIGHTNESS=0
+      fi
+    '';
+  };
+  niriDir = builtins.readDir ./niri;
+  mapFn = name: value: builtins.readFile "${./niri}/${name}";
+  combinedKdl = lib.concatMapAttrsStringSep "\n" mapFn niriDir;
+  patchedKdl = builtins.replaceStrings (builtins.attrNames variables) (builtins.attrValues variables) combinedKdl;
+in 
 {
-  xdg.configFile."niri/config.kdl".text = lib.concatMapAttrsStringSep
-    "\n\n"
-    (name: value: builtins.readFile "${./niri}/${name}")
-    (builtins.readDir ./niri);
+  xdg.configFile."niri/config.kdl".text = patchedKdl;
 }
