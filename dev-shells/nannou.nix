faf945dc8c7047f7d64862c1cf2683e9eff08db3
jonasskounielsen <jonasskou.n@gmail.com>
2025-10-22 10:55:38 +0200

    Replace processing-rs devshell with nannou one

---
+++ dev-shells/nannou.nix
@@ -0,0 +1,42 @@
+{ pkgs }:
+let
+  inherit (pkgs) lib;
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    rustPackages.clippy
+    libGL
+    libdrm
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXxf86vm
+    libxcb
+    libxkbcommon
+    vulkan-loader
+    mesa
+  ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+    rustc
+    cargo
+    libGL
+    libdrm
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXxf86vm
+    xorg.libXcursor
+    xorg.libXi
+    libxcb
+    libxkbcommon
+    vulkan-loader
+    mesa
+  ]);
+
+  shellHook = ''
+    echo "In Nannou dev shell"
+    export RUST_BACKTRACE=1
+  '';
+}
