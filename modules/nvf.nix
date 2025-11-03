af71026ba63cf5507f3fdc49953228eb2d10a7d2
jonasskounielsen <jonasskou.n@gmail.com>
2025-11-03 13:18:00 +0100

    Update nvf config

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,65 +1,88 @@
 { ... }: {
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
   programs.nvf = {
     enable = true;
-    settings = {
-      enable = true;
-      defaultEditor = true;
+    settings.vim = {
       vimAlias = true;
       viAlias = true;
-      vim = {
-        languages = {
-          enableTreesitter = true;
-          rust = {
-            enable = true;
-            crates = {
-              enable = true;
-              codeActions = true;
-            };
-            dap.enable = true;
-          };
-          nix = {
-            enable = true;
-            extraDiagnostics.enable = true;
-            lsp.server = "nixd";
-          };
-          lua = {
-            enable = true;
-          };
-        };
-
-        lsp = {
-          enable = true;        
-          inlayHints.enable = true;
-          lightbulb.enable = true;
-          lspkind.enable = true;
-          #lspsaga.enable = true;
-        };
-        
-        autocomplete.blink-cmp = {
+      
+      languages = {
+        enableTreesitter = true;
+        rust = {
           enable = true;
-          friendly-snippets.enable = true;
-        };
-
-        options = {
-          shiftwidth = 0;
-        };
-
-        #theme = {
-        #  enable = true;
-        #  
-        #};
-
-        utility = {
-          multicursors.enable = true;
-          outline.aerial-nvim.enable = true;
-          yazi-nvim = {
+          crates = {
             enable = true;
-            mappings = {
-              openYazi = "<leader>y";
-              openYaziDir = "<leader>Y";
-            };
-            setupOpts.open_for_directories = true;
+            codeActions = true;
           };
+          dap.enable = true;
+        };
+        nix = {
+          enable = true;
+          extraDiagnostics.enable = true;
+          lsp.server = "nixd";
+        };
+        lua = {
+          enable = true;
+        };
+      };
+
+      lsp = {
+        enable = true;        
+        inlayHints.enable = true;
+        lightbulb.enable = true;
+        lspkind.enable = true;
+        #lspsaga.enable = true;
+        otter-nvim.enable = true;
+        trouble.enable = true;
+      };
+
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
+
+      telescope.enable = true;
+
+      autocomplete.blink-cmp = {
+        enable = true;
+        friendly-snippets.enable = true;
+      };
+
+      options = {
+        shiftwidth = 0;
+        tabstop = 2; # TODO: differentiate between languages
+      };
+
+      statusline = {
+        lualine.enable = true;
+      };
+      
+      visuals = {
+        nvim-scrollbar.enable = true;
+        cinnamon-nvim.enable = true;
+        fidget-nvim.enable = true;
+        indent-blankline.enable = true;
+      };
+
+      #theme = {
+      #  enable = true;
+      #  
+      #};
+
+      utility = {
+        multicursors.enable = true;
+        outline.aerial-nvim.enable = true;
+        surround.enable = true;
+        yazi-nvim = {
+          enable = true;
+          mappings = {
+            openYazi = "<leader>y";
+            openYaziDir = "<leader>Y";
+          };
+          setupOpts.open_for_directories = true;
         };
       };
     };
