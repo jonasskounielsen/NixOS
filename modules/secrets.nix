e232a0a3cbbf75e1e3ff78f0f0e9da667fdbd97d
jonas <jonasskou.n@gmail.com>
2025-11-11 22:36:18 +0100

    Set (hopefully correct) declarative password

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,11 +1,16 @@
-{ ... }:
+{ config, ... }:
 {
+  # Edits secrets with: sudo SOPS_AGE_KEY_FILE=/etc/sops/age/private_key.txt sops edit secrets.yaml
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "/etc/sops/age/private_key.txt";
     secrets.rclone = {
-      owner = "jonas";
+      owner = config.users.users.jonas.name;
+    };
+    secrets.hashedPassword = {
+      neededForUsers = true;
     };
   };
+  users.users.jonas.hashedPasswordFile = config.sops.secrets.hashedPassword.path;
 }
