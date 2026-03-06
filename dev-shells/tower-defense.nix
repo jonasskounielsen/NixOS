93124e8107b04f015972f897ea751bfa129415cb
jonasskounielsen <jonasskou.n@gmail.com>
2026-03-06 08:48:08 +0100

    Add tower-defense devshell

---
+++ dev-shells/tower-defense.nix
@@ -0,0 +1,39 @@
+{ pkgs }:
+let
+  inherit (pkgs) lib;
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    rustPackages.clippy
+    libGL
+    libdrm
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXxf86vm
+    libxcb
+    libxkbcommon
+    vulkan-loader
+    mesa
+  ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+    rustc
+    cargo
+    libGL
+    libdrm
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXxf86vm
+    xorg.libXcursor
+    xorg.libXi
+    libxcb
+    libxkbcommon
+    vulkan-loader
+    mesa
+  ]);
+
+  RUST_BACKTRACE = 1;
+}
