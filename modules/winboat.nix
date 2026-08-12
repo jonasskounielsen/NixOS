a2004edd904aeebe044e686215dba7ef34df68cb
jonas <jonasskou.n@gmail.com>
2026-08-12 19:58:44 +0200

    Winboat Electron version is marked as unsafe

--- modules/winboat.nix
+++ modules/winboat.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
-  virtualisation.docker.enable = true; # For Winboat.
+  virtualisation.docker.enable = true;
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-40.10.5"
+  ];
   
   environment.systemPackages = with pkgs; [
     winboat
